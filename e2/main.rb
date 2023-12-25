d8fe6d0f60e6e7086d94c1dd2ccbfcf8bb86626c
raaghuu <sharma.raghav34@gmail.com>
2023-12-25 23:46:16 +0530

    feat: :sparkles: #E2 - Strings and Variables

---
+++ e2/main.rb
@@ -0,0 +1,11 @@
+puts 'Hello, World!'
+puts "Hello, World!"
+
+# equivalent to < print('Hello, World!') > in other languages
+
+first_name = 'Raghav'
+last_name = 'Sharma'
+
+puts first_name
+
+puts 'Hello, ' + first_name + ' ' + last_name
