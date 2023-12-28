7f8b3255f03d0147d3ea252570a54c210fceb476
raaghuu <sharma.raghav34@gmail.com>
2023-12-28 21:40:23 +0530

    feat: :sparkles: #E5 - User Input (Data Type Conversions)

--- e5/main.rb
+++ e5/main.rb
@@ -3,5 +3,10 @@ user_name = gets.chomp # chomp removes the new line character
 puts 'Hello, ' + user_name + '!'
 
 puts 'How old are you?'
-user_age = gets.chomp
-puts 'You are ' + user_age + ' years old.'
+user_age = gets.chomp.to_i
+
+puts 'You are now 1 year younger.'
+
+user_age = user_age - 1
+
+puts 'You are ' + user_age.to_s + ' years old.'
