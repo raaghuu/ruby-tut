17e572e4d4108cbd9c900629abeb654d656706d8
raaghuu <sharma.raghav34@gmail.com>
2023-12-28 21:34:09 +0530

    feat: :sparkles: #E5 - User Input

---
+++ e5/main.rb
@@ -0,0 +1,7 @@
+puts 'Hello! What\'s your name?'
+user_name = gets.chomp # chomp removes the new line character
+puts 'Hello, ' + user_name + '!'
+
+puts 'How old are you?'
+user_age = gets.chomp
+puts 'You are ' + user_age + ' years old.'
